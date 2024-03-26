e4369b5e48b5724174e52c9401840cff7b863a19
serebano <mr.serebano@gmail.com>
2024-03-26 04:38:22 +0200

    new file:   .gitignore 	new file:   cert/.env 	new file:   cert/cert.pem 	new file:   cert/cert.ts 	new file:   cert/index.ts 	new file:   cert/key.pem 	new file:   cert/key.ts 	modified:   deno.lock 	modified:   main.ts 	new file:   main2.ts

---
+++ cert/index.ts
@@ -0,0 +1,2 @@
+export * from "./key.ts";
+export * from "./cert.ts";
--- main.ts
+++ main.ts
@@ -1,20 +1,113 @@
-import { ServerOutgoingStream } from "@io/server.ts";
+// deno-lint-ignore-file require-await
+import { cert, key } from "./cert/index.ts";
+import { ServerIncomingStream, ServerOutgoingStream } from "@io/server.ts";
+import { defineHandler, serveStatic } from "@io/server.utils.ts";
 import { logHandler } from "@io/shared.logger.ts";
+import { assert } from "https://deno.land/std@0.186.0/_util/asserts.ts";
 
-Deno.serve((req) => {
-  const api = new ServerOutgoingStream(req);
+const serverOutgoingStreamHandler = defineHandler(
+  async function serverOutgoingStreamHandler(req) {
+    assert("GET" === req.method);
 
-  api.on("any", logHandler);
+    const api = new ServerOutgoingStream(req);
+    let timerId: number;
+    console.log("api", api.events);
+    api.addEventListener("close", (event) => {
+      logHandler(event);
+      clearInterval(timerId);
+      ServerOutgoingStream.write("data: the " + api.id + " was gone\n\n");
+    });
 
-  let id: number;
+    api.addEventListener("open", (event) => {
+      logHandler(event);
+      timerId = setInterval(() => {
+        api.write("data: " + Date.now() + "\n\n");
+      }, 1000);
+      ServerOutgoingStream.write("data: hello, i'm the " + api.id + "\n\n");
+    });
 
-  api.addEventListener("open", () => {
-    id = setInterval(() => api.write("hello\n"), 1000);
-  });
+    return api.response;
+  },
+);
 
-  api.addEventListener("close", () => {
-    clearInterval(id);
-  });
+const serverIncomingStreamHandler = defineHandler(
+  async function serverIncomingStreamHandler(req) {
+    assert("POST" === req.method);
 
-  return api.response;
-});
+    const api = new ServerIncomingStream(req);
+
+    api.on("open", logHandler);
+    api.on("close", logHandler);
+    api.on("error", logHandler);
+    api.on("data", logHandler);
+    api.on("end", logHandler);
+
+    // setTimeout(() => {
+    // 	api.close();
+    // }, 1000);
+
+    await api.read();
+
+    return new Response(":end", {
+      status: 200,
+    });
+  },
+);
+
+Deno.serve(
+  {
+    key,
+    cert,
+    port: 8001,
+    onError(error) {
+      console.log("error", error);
+      return new Response(String(error), { status: 400 });
+    },
+    async onListen({ port, hostname }) {
+      const url = `https://${hostname}:${port}/`;
+      console.log("\n\tListening @", url, "\n");
+
+      // const api = new ClientOutgoingStream(url);
+
+      // api.on("close", (e) =>
+      // 	console.log(
+      // 		color.blue(e.target.constructor.name),
+      // 		e.target.id,
+      // 		color.green(e.type),
+      // 		e.detail
+      // 	)
+      // );
+      // api.on("error", (e) =>
+      // 	console.log(
+      // 		color.blue(e.target.constructor.name),
+      // 		e.target.id,
+      // 		color.green(e.type),
+      // 		e.detail
+      // 	)
+      // );
+
+      // await api.ready;
+      // await api.write("Hello streams ;)");
+
+      // setTimeout(() => api.close(), 3000);
+    },
+  },
+  async (request, info) => {
+    const response = await serveStatic(request);
+    if (response instanceof Response) {
+      return response;
+    }
+
+    switch (request.method) {
+      case "GET":
+        return await serverOutgoingStreamHandler(request, info);
+      case "POST":
+        return await serverIncomingStreamHandler(request, info);
+      default: {
+        return new Response("Bad Request " + request.method, {
+          status: 400,
+        });
+      }
+    }
+  },
+);
